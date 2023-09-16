afcdf12f2156fddbef19dca8b3539dfc70f7b94f
tuva9 <tuva9@fpt.edu.vn>
2023-09-16 15:21:53 +0700

    Buổi 3: String

--- lesson2/revise/data-type.js
+++ lesson2/revise/data-type.js
@@ -3,4 +3,12 @@
 // 1. String
 let sv = "trinh ha duc sang"
 let sv2 = "vu anh tu"
-console.log(sv.toUpperCase());
+let tenSV = ""
+const result = sv.split(' ')
+for (let item of result) {
+    // console.log(item[0].toUpperCase() + item.slice(1));
+    tenSV += item[0].toUpperCase() + item.slice(1) + " "
+}
+console.log(tenSV);
+// const result = sv[0].toUpperCase() + sv.slice(1)
+// console.log(result);
