2e95df504f7c533192396f7c4f886f2e68bd63fc
tuva9 <tuva9@fpt.edu.vn>
2023-10-03 15:38:05 +0700

    Buổi 10: UseEffect

--- lesson2/src/main.js
+++ lesson2/src/main.js
@@ -1,10 +1,8 @@
 import Navigo from "navigo"
 import HomePage from "./page/homepage";
 import ProductDetail from "./page/product-detail";
-import { render } from "./lib";
+import { render, router } from "./lib";
 import './style.css'
-
-const router = new Navigo('/', {linksSelector: "a"})
 
 // Router
 router.on('/', function () {
--- lesson2/src/page/product-detail.js
+++ lesson2/src/page/product-detail.js
@@ -1,15 +1,36 @@
 import Footer from "../components/footer"
 import Header from "../components/header"
 import {books} from '../../db.json' assert {type: 'json'}
+import {useEffect} from '../lib'
 
 const ProductDetail = (id) => {
     const book = books.find(function(book) {
         return id == book.id
     })
-    return `
+
+    useEffect(function() {
+        const listThumbnail = document.querySelectorAll(".thumbnail")
+        // console.log(listThumbnail);
+        listThumbnail.forEach(function(btn) {
+            // console.log(btn.dataset.src);
+            btn.addEventListener('click', function() {
+                document.querySelector(".image-detail").src = btn.dataset.src
+            })
+        })
+
+    }, [])
+
+    return /*html*/`
     ${Header()}
     <h1>Chi tiet san pham ${id}</h1>
-    <img src="${book.images[0]}"/>
+    <img class="image-detail w-[300px]" src="${book.images[0]}"/>
+    <div class="flex gap-2">
+        ${book.images.map(function(image, index) {
+            return /*html*/`<button class="thumbnail" data-src="${image}">
+                <img class="w-[100px]" src="${image}"/>
+            </button>`
+        }).join('')}
+    </div>
     ${Footer()}
     `
 }
