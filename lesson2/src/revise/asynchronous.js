23f53e79960ad37a986b5eccfd268e69d6dbddcd
tuva9 <tuva9@fpt.edu.vn>
2023-10-05 15:37:05 +0700

    Buổi 11: Callback xử lý code Async

--- lesson2/src/revise/asynchronous.js
+++ lesson2/src/revise/asynchronous.js
@@ -8,13 +8,30 @@
 // }, 2000)
 
 // Ham bat dong bo
-function doSomeThing(congviec, time) {
-    setTimeout(function() {
-        console.log("Đã hoàn thành ",congviec);
-    }, time)
+// function doSomeThing(congviec, time) {
+//     setTimeout(function() {
+//         console.log("Đã hoàn thành ",congviec);
+//     }, time)
+// }
+
+// doSomeThing("Công việc 1", 0)
+// doSomeThing("Công việc 2", 2000)
+// doSomeThing("Công việc 3", 1000)
+// console.log("Hoàn thành công việc 4");
+
+// Bất đồng bộ
+function loadScript(src, callback) {
+    const script = document.createElement('script')
+    script.src = src
+    script.onload = function() {
+        callback()
+    }
+    document.head.append(script)
 }
 
-doSomeThing("Công việc 1", 0)
-doSomeThing("Công việc 2", 2000)
-doSomeThing("Công việc 3", 1000)
-console.log("Hoàn thành công việc 4");
+// Bất đồng bộ
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js", function() {
+    // Đồng bộ
+    console.log(_.uniq([3,5,6, 8,7, 9,8,9,10, 1]));
+})
+
