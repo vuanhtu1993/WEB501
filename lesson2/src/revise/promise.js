30fb9dc99a7abb664b28cb3431b9ea4cc1ffa9a9
tuva9 <tuva9@fpt.edu.vn>
2023-10-07 15:31:57 +0700

    Buổi 12: Fetch

--- lesson2/src/revise/promise.js
+++ lesson2/src/revise/promise.js
@@ -25,23 +25,44 @@ const promiseDoSomeThing = function (congviec, time) {
     }) 
 }
 
-promiseDoSomeThing("Cong viec 1", 1000)
-.then(function(data) {
-    console.log(data);
-    return promiseDoSomeThing("Cong viec 2", 2000)
+// promiseDoSomeThing("Cong viec 1", 1000)
+// .then(function(data) {
+//     console.log(data);
+//     return promiseDoSomeThing("Cong viec 2", 2000)
+// })
+// .then(function(data) {
+//     console.log(data);
+//     return promiseDoSomeThing("Cong viec 3", 500)
+// })
+// .then(function(data) {
+//     console.log(data);
+//     return promiseDoSomeThing("Cong viec 4", 0)
+// })
+// .then(function(data) {
+//     console.log(data);
+//     return promiseDoSomeThing("Cong viec 5", 100)
+// })
+// .catch(function(err) {
+//     console.log(err);
+// })
+
+// FETCH - promise
+
+const fetchImage = function(url) {
+    return fetch(url)
+}
+
+const renderImage = function(url) {
+    const image = document.createElement('img')
+    image.src = url
+    document.body.appendChild(image)
+}
+
+const anh1 = fetchImage('https://picsum.photos/200/300')
+anh1.then(function(data) {
+    renderImage(data.url)
+    return fetchImage("https://picsum.photos/300/400")
 })
 .then(function(data) {
-    console.log(data);
-    return promiseDoSomeThing("Cong viec 3", 500)
-})
-.then(function(data) {
-    console.log(data);
-    return promiseDoSomeThing("Cong viec 4", 0)
-})
-.then(function(data) {
-    console.log(data);
-    return promiseDoSomeThing("Cong viec 5", 100)
-})
-.catch(function(err) {
-    console.log(err);
+    renderImage(data.url)
 })
