05d0a7586978038067f1d850d996e3df2f25cab4
tuva9 <tuva9@fpt.edu.vn>
2024-01-09 10:11:44 +0700

    Buổi 4: Mdules

---
+++ lesson2/app.js
@@ -0,0 +1,13 @@
+const PI = 3.1412
+
+function log(str) {
+    console.log(str);
+}
+
+export {
+    log,
+    PI
+}
+
+export default log;
+
---
+++ lesson2/caculateP.js
@@ -0,0 +1,7 @@
+import log, {PI} from "./app.js"
+
+function circleP(r) {
+    return PI * r * 2
+}
+
+log(circleP(2))
