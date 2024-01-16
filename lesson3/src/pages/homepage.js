0bf02582f9751719d439bb7ae1e8e43c67c1aff1
tuva9 <tuva9@fpt.edu.vn>
2024-01-16 10:58:50 +0700

    Buổi 7: db.json

--- lesson3/src/pages/homepage.js
+++ lesson3/src/pages/homepage.js
@@ -2,19 +2,20 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import Card from "../components/card"
 import Advertisement from "../components/advertisement"
+
+// Data
+import { books } from '../../db.json' assert {type: 'json'}
 // Pages
 const HomePage = function () {
-    return `
+  console.log(books);
+  return `
       ${Header()}
       <h1>Trang chủ</h1>
-      <div style="display: flex; width: 100%">
-        <div style="width: 40%">${Advertisement()}</div>
-        <div style="width: 60%">
-          ${Card()}
-          ${Card()}
-          ${Card()}
-        </div>
-      </div>
+      ${books.map(function (book) {
+    return `
+          <img width="150px" src="${book.images[0].base_url}"/>
+        `
+  })}
       ${Footer()}
     `
 }
