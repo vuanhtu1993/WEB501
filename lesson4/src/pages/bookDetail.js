6789730ad945f64e86409bab31b6330110763363
tuva9 <tuva9@fpt.edu.vn>
2024-01-25 08:01:45 +0700

    Buổi 10: product detail

--- lesson4/src/pages/bookDetail.js
+++ lesson4/src/pages/bookDetail.js
@@ -1,6 +1,15 @@
+import data from '../../db.json' assert {type: "json"}
+
 const BookDetail = function (id) {
+    const book = data.books.find((item) => item.id == id)
+    console.log(book);
     return `
-        Book id = ${id}
+        <div class="flex">
+            <img class="w-1/2" src="${book.images[0].base_url}"/>
+            <div>
+                Tác giả: ${book.authors?.[0].name}
+            </div>
+        </div>
     `
 }
 
