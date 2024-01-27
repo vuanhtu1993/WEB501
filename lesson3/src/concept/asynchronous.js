d229f184bf6e3bd3ed7c35521de94df9bd68689a
tuva9 <tuva9@fpt.edu.vn>
2024-01-27 10:30:33 +0700

    Buổi 11: Promise

--- lesson3/src/concept/asynchronous.js
+++ lesson3/src/concept/asynchronous.js
@@ -19,9 +19,46 @@ function loadScript(callback) {
     document.body.appendChild(script)
 }
 
-loadScript(function () {
-    // Sync
-    const arr2 = [1, 2, 3, 4, 5, 4, 3, 2]
-    // Sync
-    console.log(_.uniq(arr2));
-})
+// loadScript(function () {
+//     // Sync
+//     const arr2 = [1, 2, 3, 4, 5, 4, 3, 2]
+//     // Sync
+//     console.log(_.uniq(arr2));
+// })
+
+//  ======= Callback =======
+// const asyncTask = function (task, time = 100, callback) {
+//     setTimeout(function () {
+//         console.log(task);
+//         callback()
+//     }, time)
+// }
+
+// asyncTask("task 1", 400)
+// asyncTask("task 2", 100)
+// asyncTask("task 3", 200)
+// asyncTask("task 4", 300)
+// Xử lý - Handle
+// asyncTask("task 1", 400, function () {
+//     asyncTask("task 2", 100, function () {
+//         asyncTask("task 3", 200, function () {
+//             asyncTask("task 4", 300)
+//         })
+//     })
+// })
+
+// ======== Promise =========
+const asyncTask = function (task, time = 100) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(task)
+        }, time)
+    })
+}
+
+asyncTask("task 1", 2000)
+    .then(result => {
+        console.log(result);
+        return asyncTask("task 2", 200)
+    })
+    .then(result => console.log(result))
